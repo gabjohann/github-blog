a0f2991e4459ebee259921ddb16d800f05eabb42
Lucas Gabriel <gabif.johann@gmail.com>
2023-09-22 23:26:42 -0300

    add: Feed Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
+import { Feed } from './pages/Feed';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <Feed />
     </ThemeProvider>
   );
 }
