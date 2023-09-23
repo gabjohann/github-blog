01c11d799d306cfac3205b52195b520f8c16d477
Lucas Gabriel <gabif.johann@gmail.com>
2023-09-23 16:40:38 -0300

    fix: header component overlow

--- components/Header/styles.ts
+++ components/Header/styles.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
-  width: 100vw;
   height: 18.5rem;
   display: flex;
+  align-items: center;
   justify-content: center;
 
   background-image: url('../../src/assets/background.png');
@@ -11,8 +11,8 @@ export const HeaderContainer = styled.header`
   background-size: cover;
 
   img {
-    margin-top: 4rem;
     width: 9.25rem;
     height: 6.125rem;
+    margin-bottom: 100px;
   }
 `;
